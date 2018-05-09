1e9330a5509cc96c9f4459b58a048909a827b919
Kelvin Jackson <kechpaja@comcast.net>
2018-05-10 01:01:27 +0300

    Navigation working much better now

--- src/js/navigate.js
+++ src/js/navigate.js
@@ -2,45 +2,35 @@ var pages = pages || {};
 
 var navigate = (function () {
 
-    function getCurrentPath() {
-        return location.pathname.replace(/(\/?keele)?\//, "");
-    }
-
-    function getCurrentPathSplit() {
-        return getCurrentPath().split("/");
-    }
-
-    function concatPath(language, lesson, section) {
-        return language + "/" + lesson + "/" + section;
-    }
-
     function loadPage(language, lesson, section) {
         if (language && lesson) {
             (pages[section] || pages["lesson"])(language, lesson);
         } 
 
         // TODO handle cases without lesson or language
+        // TODO this will mean writing index pages
     }
 
     function to(language, lesson, section) {
-        history.pushState(getCurrentPathSplit(), 
-                          null, 
-                          concatPath(language, lesson, section));
-
-        loadPage(language, lesson, section); // TODO push state here
+        // TODO replace that null?
+        history.pushState(arguments, null, arguments.join("/"));
+        loadPage.apply(this, arguments);
     }
 
 
     function loadCurrentPage() {
-        loadPage.apply(this, getCurrentPathSplit());
+        var currentPath = location.pathname.replace(/(\/?keele)?\//, "");
+        var currentPathSplit = currentPath.split("/");
 
-        // TODO do we need to replace state here? 
+        // TODO something in place of that null?
+        history.replaceState(currentPathSplit, null, currentPath);
+        loadPage.apply(this, currentPathSplit); 
     }
 
     // Set up the popstate handler
     window.addEventListener('popstate', function(e) {
         if (e.state) {
-            loadPage.apply(this, history.state);
+            loadPage.apply(this, e.state);
         }
     });
 
