a505734320a9916bf3bc78aac83dae05764918b1
Kelvin Jackson <kechpaja@comcast.net>
2018-05-11 11:53:04 +0300

    Every page is in the pages file

--- src/js/pages.js
+++ src/js/pages.js
@@ -88,4 +88,43 @@ var pages = pages || {};
         load.load(displayLesson, course, lesson);
     }
 
+
+
+
+    function displayVocab(data) {
+
+        // TODO some larger structure to put all of the items into? List?
+
+        data.items.forEach(function(item) {
+            var div = utils.createDiv("vocab-item-container");
+
+            // Item as heading
+            div.appendChild(utils.create("h2", item.item));
+           
+            // Translation in following paragraph
+            // TODO part-of-speech information?
+            // TODO joining with commas will work for words, but what
+            // about longer phrases? Perhaps we can encase them in <em> tags
+            // or something like that at some point. 
+            div.appendChild(utils.create("p", item.translations.join(",")));
+
+            // Add all images in their own container
+            var container = utils.createDiv("vocab-image-container");
+            item.images.forEach(function(imageUrl) {
+                container.append(utils.createImage("data/" + imageUrl,
+                                                   "vocab-image-class"));
+            });
+            div.appendChild(container);
+
+            // TODO Add all audio items (when they are ready), again
+            // probably in their own container
+
+            utils.anchor().appendChild(div);
+        })
+    }
+
+    pages.vocab = function (course, lesson) {
+        load.load(displayVocab, course, lesson); 
+    }
+
 })();
